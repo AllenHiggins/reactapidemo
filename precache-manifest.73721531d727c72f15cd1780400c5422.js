5c2c7d8b6528d9a13d00c0d15e90027dc54159e3
AllenHiggins <allenghiggins@gmail.com>
2018-10-19 18:05:41 +0100

    Updates

---
+++ precache-manifest.73721531d727c72f15cd1780400c5422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8febdd857fc196a1975",
+    "url": "/listapp/static/js/runtime~main.c8febdd8.js"
+  },
+  {
+    "revision": "ebd86a52d103c7a79a27",
+    "url": "/listapp/static/js/main.ebd86a52.chunk.js"
+  },
+  {
+    "revision": "6ba7967b19310a49e288",
+    "url": "/listapp/static/js/1.6ba7967b.chunk.js"
+  },
+  {
+    "revision": "ebd86a52d103c7a79a27",
+    "url": "/listapp/static/css/main.2d9d73eb.chunk.css"
+  },
+  {
+    "revision": "ed48dac0a30478db5885de1d0b6ad686",
+    "url": "/listapp/index.html"
+  }
+];
